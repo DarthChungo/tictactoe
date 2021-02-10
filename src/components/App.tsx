3095e79d9eb7882f91cdb34d536abf3f3329cc6d
DarthChungo <aharomar2004@gmail.com>
2021-02-10 23:14:01 +0100

    Created color palet and organized project

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component<React.HTMLAttributes<HTMLDivElement>, {}> {
+  render() {
+    return (
+      <div id="App" className={this.props.className}>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
